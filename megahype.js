8accc90085c5c77e56547effad16735db5158a16
James Allenspach <jima@legnog.com>
2021-08-25 19:05:27 -0500

    update everything

--- megahype.js
+++ megahype.js
@@ -1,10 +1,13 @@
-var editSpan = document.getElementById('edit-me');
-console.log(editSpan);
-editSpan.addEventListener('keypress', function(evt) {
-	console.log(evt.keyCode);
-	if (evt.keyCode == 13) {
-		document.getElementById('before-name').innerText = "before he ate a dozen hamburgers, ";
-		document.getElementById('after-name').innerText = " was a superstar on his local basketball team.";
-		evt.preventDefault();
-	}
-})
+const nameform = document.getElementById('formit');
+const hypediv = document.getElementById('hypediv');
+
+
+
+
+function logsubmit(evt) {
+    // console.log(evt);
+    hypediv.innerHTML = '<h1>Hi mom </h1>';
+    evt.preventDefault();
+}
+
+nameform.addEventListener('submit', logsubmit);
