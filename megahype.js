d0b5cb85280621971de4ca8974012614829e7223
James Allenspach <jima@legnog.com>
2021-09-02 14:29:04 -0500

    updates

--- megahype.js
+++ megahype.js
@@ -2,6 +2,11 @@ const nameform = document.getElementById('formit');
 const yourname = document.getElementById('yourname');
 const hypediv = document.getElementById('hypediv');
 
+const pronouns = {
+    "male": { "s": "he", "S": "He", "o": "him", "p": "his", "P": "His", "x": "himself" },
+    "female": { "s": "she", "S": "She", "o": "her", "p": "her", "P": "Her", "x": "herself" },
+    "plural": { "s": "they", "S": "They", "o": "them", "p": "their", "P": "Their", "x": "themself" }
+};
 var filereq = new XMLHttpRequest();
 var grammar;
 
@@ -16,7 +21,7 @@ var CHOOSECACHE = [];
 function choose(key) {
     var obj = grammar[key];
     if (! obj) {
-        console.log('key not found in grammar');
+        console.error(key,'key not found in grammar');
         return "";
     }
     if (Array.isArray(obj)) {
@@ -33,26 +38,31 @@ function choose(key) {
 
 function makeIntro() {
     str = choose('main');
-    console.log('str',str);
     newstr = str.replace(/{([^}]+)}/g, function(m,s1) { return choose(s1) });
 
     while (newstr != str) {
-        console.log(newstr);
         str = newstr;
-        newstr = str.replace(/{([^}]+)}/g, function(m,s1) { return choose(s1) });
+        newstr = str.replace(/{([^}]+)}/g, function(m,s1) { return '<span class="'+s1+'">' + choose(s1) + '</span>' });
     }
     return newstr;
 }
 
+function assignPronouns(str, name, prnkey) {
+    str = str.replace('%n', name);
+    for (var i in pronouns[prnkey]) {
+        str = str.replace(new RegExp('%'+i, 'g'), pronouns[prnkey][i]);
+    }
+    return str;
+}
 filereq.addEventListener('load', parseJson);
 filereq.addEventListener('error', errorJson);
 filereq.open('GET','megahype.json');
 filereq.send();
 
 function logsubmit(evt) {
-    grammar['name'] = yourname.value;
-    // console.log(evt);
-    hypediv.innerHTML = '<p> ' + makeIntro() + ' </p>';
+    introStr = makeIntro();
+    introStr = assignPronouns(introStr, yourname.value, 'male');
+    hypediv.innerHTML = '<p> ' + introStr + ' </p>';
     evt.preventDefault();
 }
 
